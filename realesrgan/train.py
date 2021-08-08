bef5e3cabdc07ecc8920a59652563cf1c30a608e
Xintao <wxt1994@126.com>
2021-08-08 15:27:55 +0800

    update for running

--- realesrgan/train.py
+++ realesrgan/train.py
@@ -2,10 +2,10 @@
 import os.path as osp
 from basicsr.train import train_pipeline
 
-from .archs import *
-from .data import *
-from .models import *
+import realesrgan.archs
+import realesrgan.data
+import realesrgan.models
 
 if __name__ == '__main__':
-    root_path = osp.abspath(osp.join(__file__, osp.pardir))
+    root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
     train_pipeline(root_path)
