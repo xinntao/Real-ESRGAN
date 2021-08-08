9baa0b3d0072c8c72f0c77aaf96b883bde77d18f
Xintao <wxt1994@126.com>
2021-08-08 14:46:43 +0800

    regroup files

--- realesrgan/train.py
+++ realesrgan/train.py
@@ -1,9 +1,9 @@
 import os.path as osp
 from basicsr.train import train_pipeline
 
-import archs  # noqa: F401
-import data  # noqa: F401
-import models  # noqa: F401
+import realesrgan.archs  # noqa: F401
+import realesrgan.data  # noqa: F401
+import realesrgan.models  # noqa: F401
 
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir))
