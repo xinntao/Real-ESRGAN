7de2a911ee5e8c777fb5fbdfbac61f0068d1ac19
AK391 <81195143+AK391@users.noreply.github.com>
2021-08-16 17:55:12 -0400

    Update gradiodemo.py

--- gradiodemo.py
+++ gradiodemo.py
@@ -16,7 +16,7 @@ def run_cmd(command):
     except KeyboardInterrupt:
         print("Process interrupted")
         sys.exit(1)
-run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P .")
+run_cmd("wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P experiments/pretrained_models")
 # run_cmd("pip freeze")
 run_cmd("python setup.py develop")
 def inference(img):
