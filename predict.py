1dae7d006f469a27bb801c924ad83c5be2aa93a4
Dan <dan.nelson8@gmail.com>
2023-02-15 22:43:15 +0000

    refactored & sped up

--- predict.py
+++ predict.py
@@ -1,33 +1,49 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path, File
+import os
+import tempfile
+import cv2
 from basicsr.archs.rrdbnet_arch import RRDBNet
-import os, cv2
-import subprocess
-
-subprocess.call(['python', '/src/setup.py', 'develop'])
+from cog import BasePredictor, Input, Path
+from gfpgan import GFPGANer
 
 from realesrgan import RealESRGANer
-from realesrgan.archs.srvgg_arch import SRVGGNetCompact
-from gfpgan import GFPGANer
-import tempfile
 
-model_name = 'RealESRGAN_x4plus'
-model_path = os.path.join('/root/.cache/realesrgan', model_name + ".pth")
+# subprocess.call(['python', '/src/setup.py', 'develop']) - run once at setup. 
+
+MODEL_NAME = "RealESRGAN_x4plus"
+ESRGAN_PATH = os.path.join("/root/.cache/realesrgan", MODEL_NAME + ".pth")
+GFPGAN_PATH = "/root/.cache/realesrgan/GFPGANv1.3.pth"
+
 
 class Predictor(BasePredictor):
     def setup(self):
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+        model = RRDBNet(
+            num_in_ch=3,
+            num_out_ch=3,
+            num_feat=64,
+            num_block=23,
+            num_grow_ch=32,
+            scale=4,
+        )
         netscale = 4
         self.upsampler = RealESRGANer(
             scale=netscale,
-            model_path=model_path,
+            model_path=ESRGAN_PATH,
             model=model,
             tile=0,
             tile_pad=10,
             pre_pad=0,
-            half=True)
+            half=True,
+        )
+        self.face_enhancer = GFPGANer(
+            model_path=GFPGAN_PATH,
+            upscale=4,
+            arch="clean",
+            channel_multiplier=2,
+            bg_upsampler=self.upsampler,
+        )
 
     def predict(
         self,
@@ -35,26 +51,19 @@ class Predictor(BasePredictor):
         scale: float = Input(
             description="Factor to scale image by", ge=0, le=10, default=4
         ),
-        face_enhance: bool = Input(description="Face enhance", default=True)
+        face_enhance: bool = Input(description="Run GFPGAN face enhancement along with upscaling", default=False),
     ) -> Path:
-
         img = cv2.imread(str(image), cv2.IMREAD_UNCHANGED)
-        if len(img.shape) == 3 and img.shape[2] == 4:
-            img_mode = 'RGBA'
-        else:
-            img_mode = None
 
         if face_enhance:
-            face_enhancer = GFPGANer(
-                model_path='/root/.cache/realesrgan/GFPGANv1.3.pth',
-                upscale=scale,
-                arch='clean',
-                channel_multiplier=2,
-                bg_upsampler=self.upsampler
+            print("running with face enhancement")
+            self.face_enhancer.upscale = scale
+            _, _, output = self.face_enhancer.enhance(
+                img, has_aligned=False, only_center_face=False, paste_back=True
             )
-            _, _, output = face_enhancer.enhance(img, has_aligned=False, only_center_face=False, paste_back=True)
         else:
+            print("running without face enhancement")
             output, _ = self.upsampler.enhance(img, outscale=scale)
-        save_path=os.path.join(tempfile.mkdtemp(), "output.png")
+        save_path = os.path.join(tempfile.mkdtemp(), "output.png")
         cv2.imwrite(save_path, output)
         return Path(save_path)
